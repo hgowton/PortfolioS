9505140441106603d33e49d76f2276c64bcae0af
hgowton <hgowton@gmail.com>
2020-02-22 17:22:35 -0500

    added resume, placement for projects in accordion

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,4 +1,35 @@
+
 $(document).ready(function(){
-    $("#testing").append("Nice Work!")
-    console.log("time to test")
+    $("#projectsOpt").on("click", function(project) {
+        project.preventDefault();
+        $("#about").addClass("displayNone")
+        $("#contact").addClass("displayNone")
+        $("#projectsOpt").addClass("active")
+        $("#projects").removeClass("displayNone")
+        $("#aboutOpt").removeClass("active")
+        $("#contactOpt").removeClass("active")
+
+    });
+
+    $("#contactOpt").on("click", function(contact) {
+        contact.preventDefault();
+        $("#about").addClass("displayNone")
+        $("#projects").addClass("displayNone")
+        $("#contactOpt").addClass("active")
+        $("#contact").removeClass("displayNone")
+        $("#aboutOpt").removeClass("active")
+        $("#projectsOpt").removeClass("active")
+    });
+
+    $("#aboutOpt").on("click", function(event) {
+        event.preventDefault();
+        $("#projects").addClass("displayNone")
+        $("#contacts").addClass("displayNone")
+        $("#aboutOpt").addClass("active")
+        $("#about").removeClass("displayNone")
+        $("#contactOpt").removeClass("active")
+        $("#projectsOpt").removeClass("active")
+
+    });
+
 });
