c4e38317858abe85eea0c39b43c4b80e3a103a2a
hgowton <hgowton@gmail.com>
2020-02-24 19:27:38 -0500

    displayNone and active for navigation bar

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,35 +1,108 @@
 
 $(document).ready(function(){
-    $("#projectsOpt").on("click", function(project) {
-        project.preventDefault();
-        $("#about").addClass("displayNone")
-        $("#contact").addClass("displayNone")
-        $("#projectsOpt").addClass("active")
-        $("#projects").removeClass("displayNone")
-        $("#aboutOpt").removeClass("active")
-        $("#contactOpt").removeClass("active")
 
-    });
-
-    $("#contactOpt").on("click", function(contact) {
-        contact.preventDefault();
-        $("#about").addClass("displayNone")
-        $("#projects").addClass("displayNone")
-        $("#contactOpt").addClass("active")
-        $("#contact").removeClass("displayNone")
-        $("#aboutOpt").removeClass("active")
-        $("#projectsOpt").removeClass("active")
-    });
-
-    $("#aboutOpt").on("click", function(event) {
-        event.preventDefault();
-        $("#projects").addClass("displayNone")
-        $("#contacts").addClass("displayNone")
+    $("#aboutOpt").on("click", function(about) {
+        about.preventDefault();
         $("#aboutOpt").addClass("active")
         $("#about").removeClass("displayNone")
-        $("#contactOpt").removeClass("active")
+        $("#contact").addClass("displayNone")
+        $("#projects").addClass("displayNone")
         $("#projectsOpt").removeClass("active")
-
+        $("#contactOpt").removeClass("active")
+        $("#moodOpt").removeClass("active")
+        $("#humanOpt").removeClass("active")
+        $("#giphyOpt").removeClass("active")
+        $("#triviaOpt").removeClass("active")
     });
+    
+    //fix
+    $("#projectsOpt").on("click", function(project) {
+        project.preventDefault();
+        $("#projectsOpt").addClass("active")
+        $("#projects").removeClass("displayNone")
+        $("#about").addClass("displayNone")
+        $("#contact").addClass("displayNone")
+        $("#aboutOpt").removeClass("active")
+        $("#contactOpt").removeClass("active")
+        $("#moodOpt").removeClass("active")
+        $("#humanOpt").removeClass("active")
+        $("#giphyOpt").removeClass("active")
+        $("#triviaOpt").removeClass("active")
+    });
+
+    $("#moodOpt").on("click", function(mood) {
+        mood.preventDefault();
+        $("#projectsOpt").addClass("active")
+        $("#projects").removeClass("displayNone")
+        $("#moodOpt").addClass("active")
+        $("#projects").addClass("displayNone")
+        $("#about").addClass("displayNone")
+        $("#contact").addClass("displayNone")
+        $("#aboutOpt").removeClass("active")
+        $("#contactOpt").removeClass("active")
+        $("#humanOpt").removeClass("active")
+        $("#giphyOpt").removeClass("active") 
+        $("#triviaOpt").removeClass("active")
+    });
+
+    $("#humanOpt").on("click", function(human) {
+        human.preventDefault();
+        $("#projectsOpt").addClass("active")
+        $("#projects").removeClass("displayNone")
+        $("#humanOpt").addClass("active")
+        $("#projects").addClass("displayNone")
+        $("#about").addClass("displayNone")
+        $("#contact").addClass("displayNone")
+        $("#aboutOpt").removeClass("active")
+        $("#contactOpt").removeClass("active")
+        $("#moodOpt").removeClass("active")
+        $("#giphyOpt").removeClass("active") 
+        $("#triviaOpt").removeClass("active")
+    });
+
+    $("#giphyOpt").on("click", function(giphy) {
+        giphy.preventDefault();
+        $("#projectsOpt").addClass("active")
+        $("#projects").removeClass("displayNone")
+        $("#giphyOpt").addClass("active")
+        $("#projects").addClass("displayNone")
+        $("#about").addClass("displayNone")
+        $("#contact").addClass("displayNone")
+        $("#aboutOpt").removeClass("active")
+        $("#contactOpt").removeClass("active")
+        $("#moodOpt").removeClass("active")
+        $("#humanOpt").removeClass("active") 
+        $("#triviaOpt").removeClass("active")  
+    });
+
+    $("#triviaOpt").on("click", function(trivia) {
+        trivia.preventDefault();
+        $("#projectsOpt").addClass("active")
+        $("#projects").removeClass("displayNone")
+        $("#triviaOpt").addClass("active")
+        $("#projects").addClass("displayNone")
+        $("#about").addClass("displayNone")
+        $("#contact").addClass("displayNone")
+        $("#aboutOpt").removeClass("active")
+        $("#contactOpt").removeClass("active")
+        $("#moodOpt").removeClass("active")
+        $("#giphyOpt").removeClass("active") 
+        $("#humanOpt").removeClass("active")
+    });
+
+    $("#contactOpt").on("click", function(about) {
+        about.preventDefault();
+        $("#contactOpt").addClass("active")
+        $("#contact").removeClass("displayNone")
+        $("#about").addClass("displayNone")
+        $("#projects").addClass("displayNone")
+        $("#projectsOpt").removeClass("active")
+        $("#aboutOpt").removeClass("active")
+        $("#moodOpt").removeClass("active")
+        $("#humanOpt").removeClass("active")
+        $("#giphyOpt").removeClass("active")
+        $("#triviaOpt").removeClass("active")
+    });
+
 
 });
